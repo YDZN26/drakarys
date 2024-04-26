5a357e617b660f05564af33bc5fa68e6be3955a6
YDZN26 <danizabala26@hotmail.com>
2024-04-26 10:11:34 -0400

    Vista Agregar producto 26 de abril

--- src/app/page/agregar-producto/agregar-producto.page.ts
+++ src/app/page/agregar-producto/agregar-producto.page.ts
@@ -7,8 +7,8 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./agregar-producto.page.scss'],
 })
 export class AgregarProductoPage {
-
   counterValue: number = 0; // Valor inicial del contador
+  selectedOption: string = ''; // Variable para almacenar la opción seleccionada
 
   constructor(private navCtrl: NavController) { }
 
@@ -34,7 +34,21 @@ export class AgregarProductoPage {
   increaseCounter() {
     this.counterValue++; // Incrementar el contador
   }
+
+  toggleDropdown() {
+    // Cambiar el estado del dropdown al hacer clic en el botón
+  }
+
+  onOptionChange(event: any) {
+    // Método que se ejecuta cuando cambia la opción seleccionada en el dropdown
+    this.selectedOption = event.detail.value; // Actualizar la opción seleccionada
+  }
 }
+
+
+
+
+
 
 
 
