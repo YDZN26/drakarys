016f6b4c73732587f4d9d2b570ed6ef2efc63507
YDZN26 <125135943+YDZN26@users.noreply.github.com>
2026-05-16 12:57:57 -0400

    Service Work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StatusBar, Style } from '@capacitor/status-bar';
 import { Capacitor } from '@capacitor/core';
+import { AlertController, Platform } from '@ionic/angular';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +11,18 @@ import { Capacitor } from '@capacitor/core';
 })
 export class AppComponent implements OnInit {
 
-  constructor() {}
+  alertaActualizacionAbierta = false;
+
+  constructor(
+    private swUpdate: SwUpdate,
+    private alertController: AlertController,
+    private platform: Platform
+  ) {}
 
   ngOnInit() {
     this.configurarBarraSuperior();
+    this.verificarActualizacionApp();
+    this.verificarActualizacionAlVolverApp();
   }
 
   async configurarBarraSuperior() {
@@ -29,5 +39,53 @@ export class AppComponent implements OnInit {
         style: Style.Light
       });
     }
+  }
+
+  verificarActualizacionApp() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe((event) => {
+        if (event.type === 'VERSION_READY') {
+          this.mostrarAlertaNuevaVersion();
+        }
+      });
+
+      this.swUpdate.checkForUpdate();
+    }
+  }
+
+  async mostrarAlertaNuevaVersion() {
+    if (this.alertaActualizacionAbierta) {
+      return;
+    }
+
+    this.alertaActualizacionAbierta = true;
+
+    const alert = await this.alertController.create({
+      header: 'Nueva versión disponible',
+      message: 'Se detectó una nueva actualización de la app. Actualiza para usar la última versión.',
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Actualizar',
+          handler: async () => {
+            await this.swUpdate.activateUpdate();
+            window.location.reload();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+
+    await alert.onDidDismiss();
+    this.alertaActualizacionAbierta = false;
+  }
+
+  verificarActualizacionAlVolverApp() {
+    this.platform.resume.subscribe(() => {
+      if (this.swUpdate.isEnabled) {
+        this.swUpdate.checkForUpdate();
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
@@ -15,7 +15,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,11 +29,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatDatepickerModule,
     MatFormFieldModule,
     MatNativeDateModule,
-    MatInputModule
+    MatInputModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-
